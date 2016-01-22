3bcec33492be0f670712fc8533a16dfc1850a059
plgruener <pl.gruener@gmail.com>
2016-01-22 21:51:04 +0100

    playing with the LEDs

--- keyboard/ergodox_ez/keymaps/wurzel-neo/keymap.c
+++ keyboard/ergodox_ez/keymaps/wurzel-neo/keymap.c
@@ -242,10 +242,13 @@ void * matrix_scan_user(void) {
     ergodox_right_led_3_off();
     switch (layer) {
       // TODO: Make this relevant to the ErgoDox EZ.
-        case 1:
-            ergodox_right_led_1_on();
+		case SHFT:
+			ergodox_right_led_1_on();
+			break;
+        case NAVI:
+            ergodox_right_led_3_on();
             break;
-        case 2:
+        case PROG:
             ergodox_right_led_2_on();
             break;
         default:
