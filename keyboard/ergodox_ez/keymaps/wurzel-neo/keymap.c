6d3bb835e65c98bacae4d163ca610f0c4302e522
plgruener <pl.gruener@gmail.com>
2016-01-22 12:00:10 +0100

    add Fn-keys on Nav-Layer

--- keyboard/ergodox_ez/keymaps/wurzel-neo/keymap.c
+++ keyboard/ergodox_ez/keymaps/wurzel-neo/keymap.c
@@ -162,9 +162,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 2: Media and mouse keys
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      | Bcksp|  Up  |  Del |      |      |           |      |      |   7  |   8  |   9  |      |        |
+ * |        |      | Bcksp|  Up  |  Del |      |      |           |      |      |   7  |   8  |   9  |      |   F12  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        | Home | Left | Down | Right|  End |------|           |------|      |   4  |   5  |   6  |   ,  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -182,7 +182,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // Navigation Keys + Numpad
 [NAVI] = KEYMAP(
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,
        KC_TRNS, KC_TRNS, KC_BSPC, KC_UP,   KC_DELT, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,
        KC_TRNS, KC_ESC,  KC_TRNS, KC_TRNS, KC_ENT,  KC_TRNS, KC_TRNS,
@@ -191,8 +191,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                     KC_TRNS,
                                   KC_TRNS, KC_TRNS, KC_TRNS,
     // right hand
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS,  KC_TRNS, KC_7,    KC_8,    KC_9,    KC_TRNS, KC_TRNS,
+       KC_TRNS,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+       KC_TRNS,  KC_TRNS, KC_7,    KC_8,    KC_9,    KC_TRNS, KC_F12,
                  KC_TRNS, KC_4,    KC_5,    KC_6,    KC_COMM, KC_TRNS,
        KC_TRNS,  KC_TRNS, KC_1,    KC_2,    KC_3,    KC_DOT,  KC_TRNS,
                           KC_0,    KC_NO,   KC_NO,   KC_TRNS, KC_TRNS,
