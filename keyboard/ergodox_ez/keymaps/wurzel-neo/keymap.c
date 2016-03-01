58c796ad71dfc45efececabb536f14d675d19726
plgruener <pl.gruener@gmail.com>
2016-03-02 00:34:38 +0100

    add fn keys on base layer in number row

--- keyboard/ergodox_ez/keymaps/wurzel-neo/keymap.c
+++ keyboard/ergodox_ez/keymaps/wurzel-neo/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: lowercase-letters layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   Esc  |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |        |
+ * |   Esc  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |           |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   F20  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |    y   |   x  |   v  |   l  |   c  |   w  |  Del |           |  Ins |   k  |   h  |   g  |   f  |   q  |    ß   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_ESC,     NEO_1,      NEO_2,      NEO_3,   NEO_4,   NEO_5,   KC_TRNS,
+        KC_ESC,     KC_F1,      KC_F2,      KC_F3,   KC_F4,   KC_F5,   KC_F6,
         NEO_Y,      NEO_X,      NEO_V,      NEO_L,   NEO_C,   NEO_W,   KC_DELT,
         KC_TAB,     NEO_U,      NEO_I,      NEO_A,   NEO_E,   NEO_O,
         KC_TRNS,    NEO_UE,     NEO_OE,     NEO_AE,  NEO_P,   NEO_Z,   KC_TRNS,
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                      KC_LALT,
                                              LT(NAVI,KC_ENT),KC_LSFT,NEO_L1_L,
         // right hand
-        KC_TRNS,     NEO_6,     NEO_7,     NEO_8,     NEO_9,     NEO_0,     NEO_MINS,
+        KC_F7,       KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12,    KC_F20,
         KC_INS,      NEO_K,     NEO_H,     NEO_G,     NEO_F,     NEO_Q,     NEO_SS,
                      NEO_S,     NEO_N,     NEO_R,     NEO_T,     NEO_D,     NEO_Y,
         KC_TRNS,     NEO_B,     NEO_M,     NEO_COMM,  NEO_DOT,   NEO_J,     KC_TRNS,
